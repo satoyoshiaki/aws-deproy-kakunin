aa35ea054240b4b8529f29e6436f327eb5ebbf02
佐藤良昭 <yoshiprograming@gmail.com>
2020-07-30 22:19:57 +0900

    rootの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :awsdes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "awsdes#index"
 end
